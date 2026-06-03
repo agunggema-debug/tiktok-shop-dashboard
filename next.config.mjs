38357e4a1b167a67cee0a80086ffc00752ed21fe
Your Name <agunggema@gmail.com>
2026-06-03 13:17:40 +0700

    fix: add trailingSlash and .nojekyll for github pages routing

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // Wajib untuk GitHub Pages
+  output: "export", // Wajib untuk GitHub
+  trailingSlash: true, // Wajib untuk GitHub Pages
   images: {
     unoptimized: true, // Wajib diletakkan di tingkat atas
   },
+
   basePath: process.env.NODE_ENV === "production" ? "/tiktok-shop-dashboard" : "",
 };
 
