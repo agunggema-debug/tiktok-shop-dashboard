8fb4e4f6e367b58628ef7b0e93c4fd2c17d50e86
Your Name <agunggema@gmail.com>
2026-06-03 11:40:27 +0700

    initial commit: upgrade to Node.js 24 and fallbacks for dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,9 @@
 const nextConfig = {
   output: "export", // Wajib untuk GitHub Pages
   images: {
-    unoptimized: true, // Wajib diletakkan di tingkat atas, bukan di dalam 'experimental'
+    unoptimized: true, // Wajib diletakkan di tingkat atas
   },
   basePath: process.env.NODE_ENV === "production" ? "/tiktok-shop-dashboard" : "",
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
 };
 
 export default nextConfig;
